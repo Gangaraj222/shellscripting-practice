ef09cd892ade1d7a54575e3c341eb435768396a4
Gangaraju <ug.raju11@gmail.com>
2025-12-14 22:54:43 +0530

    changes commited

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,5 +6,5 @@ PERSON2=$2
 echo "$PERSON1: Hey $PERSON2, How are you?"
 echo "$PERSON2: Hello $PERSON1, I am fine. how are you doing"
 echo "$PERSON1: I am fine too. what's up?"
-echo ":$PERSON2: Nothing, just going to mars now, will you come?"
+echo "$PERSON2: Nothing, just going to mars now, will you come?"
 echo "$PERSON1: sorry, you carry on! I will come once you come back"
