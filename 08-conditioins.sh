54b82982880170c4832be528d0c2b1504965cc5b
Gangaraju <ug.raju11@gmail.com>
2025-12-14 23:10:06 +0530

    changes commited

--- 08-conditioins.sh
+++ 08-conditioins.sh
@@ -4,8 +4,8 @@ NUMBER=$5
 
 if [ $NUMBER -gt 10 ]
 then
-echo "Given NUMBER is less than 10"
+echo "Given NUMBER is greater than 10"
 else
-echo "Given NUMBER is grater than 10"
+echo "Given NUMBER is less than 10"
 fi
 
